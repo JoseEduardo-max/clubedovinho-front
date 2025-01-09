d933da6837791b47c60733ac85435a2065e5e442
JoseEduardo-max <jeduardoff2@gmail.com>
2025-01-09 19:25:40 -0300

    adicionei o js da data

---
+++ js/dia.js
@@ -0,0 +1,25 @@
+const SELECT_DIA = document.getElementById('select-dia') // JEITO MAIS SENIOR
+const SELECT_MES = document.getElementById('select-mes')
+
+let numero = 1;
+
+while (numero <= 31) {
+    //document.getElementById('select-dia').innerHTML += '<option>' + numero + '</opition>';
+    SELECT_DIA.innerHTML += `<option>
+        ${numero}
+    </opition>`;  
+
+    numero = numero + 1;
+    //numero += 1;
+    //numero++;
+}
+
+let mes = 1
+
+do {
+    SELECT_MES.innerHTML += `<option>${mes}</option>`;
+    
+    mes = mes + 1
+
+}while (mes <= 12); 
+

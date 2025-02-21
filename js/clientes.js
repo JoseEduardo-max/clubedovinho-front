eecd97ebfc233cd578836c8ded75c362c0e016aa
JoseEduardo-max <jeduardoff2@gmail.com>
2025-02-21 00:53:02 -0300

    limpando o codigo

--- js/clientes.js
+++ js/clientes.js
@@ -8,16 +8,23 @@ fetch(url)                  //busca
         for (let i = 0; i <= clientes.length; i++) {
             TABLE_CLIENTES.innerHTML +=
                 `<tr>
-                <th>${clientes[i].id}</th>
-                <th>${clientes[i].nome}</th>
-                <th>${clientes[i].email}</th>
-                <th>${clientes[i].telefone}</th>
-                <th><img src="https://thispersondoesnotexist.com/" width="50"></th>
-                <th>${clientes[i].data_cadastro}</th>
-                <th>${clientes[i].data_ultimo_pedido}</th>
-            </tr>`
-        }
-    });
+                    <th>${clientes[i].id}</th>
+                    <th>${clientes[i].nome}</th>
+                    <th>${clientes[i].email}</th>
+                    <th>${clientes[i].telefone}</th>
+                    <th><img src="https://thispersondoesnotexist.com/" width="50"></th>
+                    <th>${clientes[i].data_cadastro}</th>
+                    <th>${clientes[i].data_ultimo_pedido}</th>
+                    <td>
+                        <a href="#" class="btn btn-outline-warning btn-sm">
+                            Editar
+                        </a>
+                        <a href="#" class="btn btn-outline-danger btn-sm">
+                            Excluir
+                        </a>
+                    </td>
+                </tr>`
+        }});
 
     
     //outro modo de fazer
